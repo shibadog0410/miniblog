00edbd402781eb97ae5e77f9e57d4d5e5eea3f90
shiba <shibadog0410@gmail.com>
2021-01-17 15:17:12 +0900

    modify start script

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
     res.render('index.ejs');
-}).listen(PORT, () => console.log(`Listening on ${ PORT }`))
+}).listen(PORT, () => console.log(`Listening on ${ PORT }`));
