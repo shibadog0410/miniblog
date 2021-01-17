34c21c7a71042c477ead04f3e16b2a1f1ac0bd85
shiba <shibadog0410@gmail.com>
2021-01-17 15:46:07 +0900

    fix: express statement

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
-const app = express()
 const PORT = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-    res.render('index.ejs');
-}).listen(PORT, () => console.log(`Listening on ${ PORT }`));
+express()
+    .get('/', (req, res) => {
+        res.render('index.ejs');
+    }).listen(PORT, () => console.log(`Listening on ${ PORT }`));
