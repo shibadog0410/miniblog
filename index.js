433e6c3f6205bfbd58ebb113a3010a482e37afd8
shiba <shibadog0410@gmail.com>
2021-01-17 15:03:58 +0900

    set listen PORT

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const app = express();
-
-const server = app.listen(5000);
+const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
     res.render('index.ejs');
-});
+}).listen(PORT, () => console.log(`Listening on ${ PORT }`))
